016d230195ac1450192c8ef6cfc39fe43dde29cb
Elanna <elannaesilva@gmail.com>
2023-05-08 19:26:04 -0300

    insere a nova versao do projeto integrado com o mongodb

---
+++ mulherModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const MulherSchema = new mongoose.Schema({
+    nome: {
+       type: String,
+       require: true
+},
+    imagem: {
+        type: String,
+        require: true
+    },
+    minibio: {
+        type: String,
+        require: true
+        }
+
+})
+
+module.exports = mongoose.model('diva', MulherSchema)
--- mulheres.js
+++ mulheres.js
@@ -1,35 +1,90 @@
-const express = require ('express')
-const router = express.Router()
+const express = require ('express')  //aqui estou inciando o express
+const router = express.Router() //aqui estou config a primeira parte da rota
+const cors = require('cors') //aqui estou tazendo o pacote cors que permite consumir essa API no front-end
+const conectaBancoDeDados = require('./bancoDeDados') //aqui estou ligando o banco de dados ao arquivo bancoDedados
+conectaBancoDeDados() //aqui estou chamando a função que conecta o banco de dados
+const Mulher = require('./mulherModel')
+const app = express() //aqui estou iniciando o app
+app.use(express.json())
+app.use(cors())
+const porta = 3333 //aqui estou criando a porta
 
-const app = express()
-const porta = 3333
 
-const mulheres = [
-    {
-            nome: 'Elanna Silva',
-            imagem: 'https://media.licdn.com/dms/image/C4E03AQHru3QpUAabAg/profile-displayphoto-shrink_400_400/0/1652814954518?e=1686787200&v=beta&t=lUxNHVE6TQXwPOn-XNmtNui-qY2T6rLFlnEPdDuhRYw',
-            minibio: 'Cozinheira por amor, futura desenvolvedora'
-        }, 
-        {
-            nome: 'Mulher 2',
-            imagem: 'https://media.licdn.com/dms/image/C4E03AQHru3QpUAabAg/profile-displayphoto-shrink_400_400/0/1652814954518?e=1686787200&v=beta&t=lUxNHVE6TQXwPOn-XNmtNui-qY2T6rLFlnEPdDuhRYw',
-            minibio: 'Bio 2'
-        },
-        {
-            nome: 'Mulher 3',
-            imagem: 'https://media.licdn.com/dms/image/C4E03AQHru3QpUAabAg/profile-displayphoto-shrink_400_400/0/1652814954518?e=1686787200&v=beta&t=lUxNHVE6TQXwPOn-XNmtNui-qY2T6rLFlnEPdDuhRYw',
-            minibio: 'Bio 3'
-        }
+//função criada pro GET
+async function mostraMulheres(request, response) {
+    try {
+        const mulheresvindasdoBancoDeDados = await Mulher.find()
+
+        response.json(mulheresvindasdoBancoDeDados)
+
+    }catch (erro) {
+        console.log(erro)
+
+    }
     
-]
+}
+//POST
+async function criaMulher(request, response) {
+     const novaMulher = new Mulher({
+       
+        nome: request.body.nome,
+        imagem: request.body.imagem,
+        minibio: request.body.minibio
+     })
 
-function mostraMulheres(request, response) {
-    response.json(mulheres)
+     try {
+        const mulherCriada = await novaMulher.save()
+        response.status(201).json(mulherCriada)
+     }catch (erro){
+        console.log(erro)
+     }
 }
 
+//PATCH
+async function corrigeMulher(request, response) {
+    try{
+        const mulherEncontrada = await Mulher.findById(request.params.id)
+
+        if (request.body.nome) {
+            mulherEncontrada.nome = request.body.nome
+        }
+    
+        if (request.body.minibio) {
+            mulherEncontrada.minibio = request.body.minibio
+        }
+    
+        if (request.body.imagem) {
+            mulherEncontrada.imagem = request.body.imagem
+        }
+
+        const mulherAtualizadaNoBancoDeDados = await mulherEncontrada.save()
+        response.json(mulherAtualizadaNoBancoDeDados)
+
+    }catch(erro){
+        console.log(erro)
+    }
+    
+} 
+
+//PORTA
 function mostraPorta () {
     console.log ('Servidor criado e rodando porta ', porta)
 }
 
-app.use(router.get('/mulheres', mostraMulheres))
-app.listen(porta, mostraPorta)
+//DELETE
+async function deletaMulher(request, response){
+    try{
+        await Mulher.findByIdAndDelete(request.params.id)
+        response.json({ mensagem:'Mulher deletada com sucesso!'})
+
+    }catch(erro){
+        console.log(erro)
+    }
+
+}
+
+app.use(router.get('/mulheres', mostraMulheres)) //configurei rota GET /mulheres
+app.use(router.post('/mulheres', criaMulher)) //configurei rota POST /mulheres
+app.listen(porta, mostraPorta) //servidor ouvindo a porta
+app.use(router.patch('/mulheres/:id', corrigeMulher)) //config a rota PATCH /mulheres/:id
+app.use(router.delete('/mulheres/:id', deletaMulher)) //config a rota DELETE /mulheres
